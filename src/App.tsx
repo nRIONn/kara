e46c4a2bd2da17b99312eb312b645011d2413480
blueflame503 <blue.flame503@gmail.com>
2023-02-13 15:32:57 +0900

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,49 @@
-import { AppBar, Box, Button, Container, Paper, Stack, TextField, Typography } from '@mui/material';
+import { Box, Button, Container, Stack, TextField, Typography } from '@mui/material';
 import { useState } from 'react'
 import Bar from './Bar';
 
+const defaultUrl = "https://www.joysound.com/web/karaoke/ranking/age/ranking?age=1990&startIndex=0&search=1990#jp-cmp-main"
+
 function App() {
-  const [max, setMax] = useState(0)
-  const [min, setMin] = useState(0)
+  const [max, setMax] = useState(500)
+  const [min, setMin] = useState(1)
   const [rand, setRand] = useState(0)
+  const [url, setUrl] = useState(defaultUrl)
 
   const createRandom = () => {
     setRand(Math.ceil(Math.random() * (max - min + 1)) + min - 1)
+    // 乱数の正確性確認
     // let sum = 0
     // for (let i = 0; i < 100000000; i++)
     //   sum += Math.ceil(Math.random() * (max - min + 1)) + min - 1
     // console.log(sum)
+
+    createUrl()
   }
 
+  const createUrl = () => {
+    const urlObj = new URL(url)
+    const stIdx = (Math.ceil(rand / 50) - 1) * 50
+    // start
+    urlObj.search = urlObj.search.split('&').map((param) => {
+      if (param.includes('startIndex')) {
+        return `startIndex=${stIdx}`
+      } else {
+        return param
+      }
+    }).join('&')
+    setUrl(urlObj.href)
+    console.dir(urlObj)
+  }
+
+  /** 
+   * 機能
+   * ・乱数が振れる
+   * ・リンクを出す
+   * ・履歴保存（24時間？）
+   * ・
+   * https://www.joysound.com/web/karaoke/ranking/age/ranking?age=1990&startIndex=50&search=1990#jp-cmp-main
+   */
 
   return (
     <div>
@@ -32,6 +61,8 @@ function App() {
           </Stack>
           <Button sx={{ width: '100%' }} variant="contained" onClick={createRandom}>生成</Button>
         </Stack>
+        <TextField label="base url" value={min} type="number" onChange={(e) => setMin(Number(e.target.value))} />
+        <Button onClick={() => window.open(url)}>開く</Button>
       </Container>
     </div >
   );
--- src/Bar.tsx
+++ src/Bar.tsx
@@ -1,10 +1,7 @@
-import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
 export default function Bar() {
     return (
         <Box sx={{ flexGrow: 1 }}>
@@ -12,9 +9,8 @@ export default function Bar() {
                 <Toolbar>
 
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        News
+                        カラオケ
                     </Typography>
-                    <Button color="inherit">Login</Button>
                 </Toolbar>
             </AppBar>
         </Box>
