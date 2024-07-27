b0b8407c4e661d3bba8032d5e4d30b7f4e6aeb82
RionNishikawa <blue.flame503@gmail.com>
2024-07-27 21:00:14 +0900

    a

--- src/Karaoke.tsx
+++ src/Karaoke.tsx
@@ -8,18 +8,18 @@ import {
   TextareaAutosize,
   TextField,
   Typography,
-} from "@mui/material";
-import { useEffect, useState } from "react";
+} from "@mui/material"
+import { useEffect, useState } from "react"
 import {
   generateHistoryElements,
   getSongName,
   History,
   intializeHistory,
   storeHistory,
-} from "./history";
-import RemoveDialog from "./RemoveDialog";
+} from "./history"
+import RemoveDialog from "./RemoveDialog"
 const defaultUrl =
-  "https://www.joysound.com/web/karaoke/ranking/age/ranking?age=1995&startIndex=0";
+  "https://www.joysound.com/web/karaoke/ranking/age/ranking?age=1995&startIndex=0"
 
 /**
  * 機能
@@ -29,79 +29,83 @@ const defaultUrl =
  * ・年代±10機能
  */
 function Karaoke() {
-  const [max, setMax] = useState(500);
-  const [min, setMin] = useState(1);
-  const [age, setAge] = useState(1995);
-  const [rand, setRand] = useState(0);
-  const [duplicationTitle, setDuplication] = useState("");
-  const [history, setHistory] = useState<History>({ list: [] });
-  const [url, setUrl] = useState(defaultUrl);
-  const [openRemoveDialpg, setOpenRemoveDialog] = useState(false);
+  const [max, setMax] = useState(500)
+  const [min, setMin] = useState(1)
+  const [age, setAge] = useState(1995)
+  const [rand, setRand] = useState(0)
+  const [duplicationTitle, setDuplication] = useState("")
+  const [history, setHistory] = useState<History>({ list: [] })
+  const [url, setUrl] = useState(defaultUrl)
+  const [openRemoveDialpg, setOpenRemoveDialog] = useState(false)
 
   const createRandom = async () => {
-    const number = Math.ceil(Math.random() * (max - min + 1)) + min - 1;
-    const title = await getSongName(number, age);
-    const duplictate = history.list.some((li) => li.title === title);
+    try {
+      const number = Math.ceil(Math.random() * (max - min + 1)) + min - 1
+      const title = await getSongName(number, age)
+      const duplictate = history.list.some((li) => li.title === title)
 
-    // 重複確認して履歴に追加
-    if (!duplictate) {
-      const newHistory = {
-        number: number,
-        time: new Date().getTime(),
-        age,
-        title,
-      };
-      setHistory({ list: [...history.list, newHistory] });
-      setDuplication("");
-    } else {
-      // 重複表示
-      setDuplication(title);
+      // 重複確認して履歴に追加
+      if (!duplictate) {
+        const newHistory = {
+          number: number,
+          time: new Date().getTime(),
+          age,
+          title,
+        }
+        setHistory({ list: [...history.list, newHistory] })
+        setDuplication("")
+      } else {
+        // 重複表示
+        setDuplication(title)
+      }
+
+      setRand(number)
+    } catch (e) {
+      console.error(e)
     }
-
-    setRand(number);
-  };
+  }
 
   const changeAge = () => {
-    setAge(Math.ceil(Math.random() * (1984 - 2006 + 1)) + 2006 - 1);
-  };
+    setAge(Math.ceil(Math.random() * (1984 - 2006 + 1)) + 2006 - 1)
+  }
   const createUrl = () => {
-    const urlObj = new URL(defaultUrl);
-    const stIdx = Math.floor((rand - 1) / 50) * 50;
+    const urlObj = new URL(defaultUrl)
+    const stIdx = Math.floor((rand - 1) / 50) * 50
     // start
     urlObj.search = urlObj.search
       .split("&")
       .map((param) => {
         if (param.includes("startIndex")) {
-          return `startIndex=${stIdx}`;
+          return `startIndex=${stIdx}`
         } else if (param.includes("age")) {
-          return `age=${age}`;
+          return `age=${age}`
         } else {
-          return param;
+          return param
         }
       })
-      .join("&");
-    setUrl(urlObj.href);
-  };
+      .join("&")
+    setUrl(urlObj.href)
+  }
 
-  useEffect(createUrl, [rand, age]);
-  useEffect(() => storeHistory(history), [history]);
+  useEffect(createUrl, [rand, age])
+  useEffect(() => storeHistory(history), [history])
 
   // 履歴の初期取得
-  useEffect(() => setHistory(intializeHistory()), []);
+  useEffect(() => setHistory(intializeHistory()), [])
 
-  const historyList: JSX.Element[] = generateHistoryElements(history);
+  const historyList: JSX.Element[] = generateHistoryElements(history)
 
   const deleteHistory = () => {
-    history.list.shift();
-    setHistory({ list: [...history.list] });
-    setOpenRemoveDialog(false);
-  };
+    history.list.shift()
+    setHistory({ list: [...history.list] })
+    setOpenRemoveDialog(false)
+  }
 
   const deleteAllHistory = () => {
-    localStorage.clear();
-    setHistory({ list: [] });
-    setOpenRemoveDialog(false);
-  };
+    localStorage.clear()
+    setHistory({ list: [] })
+    setOpenRemoveDialog(false)
+  }
 
   return (
     <div>
@@ -195,7 +199,7 @@ function Karaoke() {
         deleteAll={deleteAllHistory}
       />
     </div>
-  );
+  )
 }
 
-export default Karaoke;
+export default Karaoke
